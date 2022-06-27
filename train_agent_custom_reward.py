5f5064c6a957f718c42f4381a331c4ef3a94e363
Robin Müller <robin.mueller@aclue.de>
2022-06-27 19:35:37 +0200

    fix dynamic q-table

--- train_agent_custom_reward.py
+++ train_agent_custom_reward.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import itertools
 import random
 import time
@@ -6,43 +7,53 @@ import flappy_bird_gym
 import numpy as np
 from IPython.display import clear_output
 
-DATA_DIVIDER = 10
-
 # Hyperparameters
 ALPHA = 0.7  # learning rate (0.7 to start, reduce over time)
 GAMMA = 0.2  # discount factor
-EPSILON = 0.0  # exploration rate (0 to start, increase over time)
+EPSILON = 0.1  # exploration rate (0.1 to start, adjust over time)
 
 
-def h_v_dist():
-    screen_size = env._screen_size
-    h_dist = int(screen_size[0] / DATA_DIVIDER)
-    v_dist = int(screen_size[1] / DATA_DIVIDER)
-    return h_dist, v_dist
-
-
-def load_data(h_dist, v_dist):
+def load_data():
     try:
-        q_table = np.load(f"q_table_custom_{DATA_DIVIDER}.npy")
+        q_table = np.load(f"q_table_custom.npy")
         print("q_table loaded")
     except FileNotFoundError:
-        q_table = np.zeros([(h_dist + 1) * (v_dist + 1) * 2, 2])
+        q_table = np.array([])
         print("q_table created")
     return q_table
 
 
-def transform_state(h_dist, v_dist, state):
-    y_values = np.arange(-v_dist, v_dist + 1, 1)
-    x_values = np.arange(0, h_dist + 1, 1)
+def transform_state(state):
+    screen_size = env._screen_size
 
-    state_permutations = list(itertools.product(x_values, y_values))
+    x = (state[0] * screen_size[0]).round(0)
+    y = (state[1] * screen_size[1]).round(0)
 
-    x = (state[0] * h_dist).clip(min=0, max=h_dist).round(0)
-    y = (state[1] * v_dist).clip(min=-v_dist, max=v_dist).round(0)
+    return np.array([x, y])
 
-    transformed_state = np.array([x, y])
-    return state_permutations.index(
-        (transformed_state[0], transformed_state[1]))
+
+def get_state_index(state, q_table):
+    if len(q_table) == 0:
+        return None
+
+    transformed_state = transform_state(state)
+    return np.where(np.all(q_table[:, 0] == transformed_state, axis=1))
+
+
+def add_or_get_state_in_q_table(state, q_table):
+    state_index = get_state_index(state, q_table)
+
+    if state_index is not None and len(q_table[state_index]) > 0:
+        return np.argmax(q_table[state_index, 1]), state_index, q_table
+    else:
+        q_values = [0, 0]
+        if len(q_table) == 0:
+            q_table = np.array([[transform_state(state), q_values]])
+        else:
+            q_table = np.append(
+                q_table, [[transform_state(state), q_values]], axis=0)
+        # default action for new state
+        return 1, len(q_table) - 1, q_table
 
 
 def render_game(enabled=False):
@@ -55,11 +66,8 @@ if __name__ == "__main__":
     # make gym environment
     env = flappy_bird_gym.make("FlappyBird-v0")
 
-    # init state with reduced complexity
-    h_dist, v_dist = h_v_dist()
-
     # load or create q-table
-    q_table = load_data(h_dist, v_dist)
+    q_table = load_data()
 
     # for logging
     epoch_history = []
@@ -67,25 +75,23 @@ if __name__ == "__main__":
     for i in range(1, 10001):
         # reset environment
         state = env.reset()
-        # get state index for q-table
-        state_index = transform_state(h_dist, v_dist, state)
 
+        state_index = None
         epochs, reward, = 0, 0
         done = False
 
         while not done:
+
+            # Exploit learned values
+            action, state_index, q_table = add_or_get_state_in_q_table(
+                state, q_table)
+            # Explore action space
             if random.uniform(0, 1) < EPSILON:
-                # Explore action space
                 action = env.action_space.sample()
-            else:
-                # Exploit learned values
-                action = np.argmax(q_table[state_index])
 
             # perform chosen action
             next_state, _, done, info = env.step(
                 action)
-            # get next state index for q-table
-            next_state_index = transform_state(h_dist, v_dist, next_state)
 
             # calculate custom reward
             reward = 0
@@ -93,22 +99,26 @@ if __name__ == "__main__":
                 reward = -1000
 
             # get old value in q-table
-            old_value = q_table[state_index][action]
+            old_value = float(q_table[state_index, 1, action])
+
             # determine best action for next state
-            next_max = np.max(q_table[next_state_index])
+            next_state_index = get_state_index(next_state, q_table)
+            next_state_actions = q_table[next_state_index]
+            next_max = np.max(
+                next_state_actions[:, 1]) if len(next_state_actions) > 0 else 0
 
             # calculate q-function
-            new_value = (1 - ALPHA) * old_value + ALPHA * \
-                (reward + GAMMA * next_max)
+            new_value = float((1 - ALPHA) * old_value + ALPHA *
+                              (reward + GAMMA * next_max))
             # update q-table
-            q_table[state_index][action] = new_value
+            q_table[state_index, 1, action] = new_value
 
             # advance state for next loop
             state = next_state
             epochs += 1
 
             # Render the game (should be disabled in training)
-            render_game(True)
+            render_game(False)
 
         # for logging
         epoch_history.append(epochs)
@@ -119,6 +129,6 @@ if __name__ == "__main__":
             print(
                 f"(saved) | Episode: {i} | Epochs: min: {min(epoch_history)}, avg: {sum(epoch_history) / len(epoch_history)}, max: {max(epoch_history)}")
             epoch_history = []
-            np.save(f'q_table_{DATA_DIVIDER}', q_table)
+            np.save(f'q_table_custom', q_table)
 
     print("Training finished.\n")
