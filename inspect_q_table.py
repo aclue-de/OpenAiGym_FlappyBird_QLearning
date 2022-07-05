5aed24af08aed73a4e2bc908acdee70cc7c4c6f2
Robin Müller <robin.mueller@aclue.de>
2022-07-05 08:04:52 +0200

    cleanup

--- inspect_q_table.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from ast import literal_eval
-from pandas import read_csv
-
-q_table = read_csv("q_table.csv", names=["state_history", "actions"])
-
-print(f"shape: {q_table.shape}")
